c47594236884c02918476ccbe3df7042cd69f7c5
bgarcia95 <bgarcia.95@outlook.com>
2019-10-30 13:36:01 -0600

    Showing movies' posters on swiper

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
 
 class CardSwiper extends StatelessWidget {
-  final List<dynamic> peliculas;
+  final List<Pelicula> peliculas;
 
   CardSwiper({@required this.peliculas});
 
@@ -20,8 +21,9 @@ class CardSwiper extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return ClipRRect(
             borderRadius: BorderRadius.circular(20.0),
-            child: Image.network(
-              "https://larepublica.pe/resizer/AE4Cl6n8InP7xuQeJXm0WZarunM=/1200x660/top/arc-anglerfish-arc2-prod-gruporepublica.s3.amazonaws.com/public/NWL5VBI4A5FKHDK7ML3QYDCH7U.jpg",
+            child: FadeInImage(
+              image: NetworkImage(peliculas[index].getPosterImg()),
+              placeholder: AssetImage('assets/img/no-image.jpg'),
               fit: BoxFit.cover,
             ),
           );
