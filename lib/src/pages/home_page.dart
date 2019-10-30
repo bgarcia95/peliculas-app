93885131f799833bc13e480587a550dd3c960fae
bgarcia95 <bgarcia.95@outlook.com>
2019-10-30 16:55:34 -0600

    adding stream builder into the project

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -9,6 +9,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    peliculasProvider.getPopulares();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Peliculas en cines'),
@@ -61,12 +64,14 @@ class HomePage extends StatelessWidget {
             ),
           ),
           SizedBox(height: 5.0),
-          FutureBuilder(
-            future: peliculasProvider.getPopulares(),
-            // initialData: InitialData,
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
               if (snapshot.hasData) {
-                return MovieHorizontal(peliculas: snapshot.data);
+                return MovieHorizontal(
+                    peliculas: snapshot.data,
+                    // Volviendo a llamar la funcion para obtener nueva pagina
+                    siguientePagina: peliculasProvider.getPopulares);
               } else {
                 return Center(child: CircularProgressIndicator());
               }
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -2,20 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 
 class MovieHorizontal extends StatelessWidget {
-  
-  final List<Pelicula> peliculas;
 
-  MovieHorizontal({@required this.peliculas});
+  final List<Pelicula> peliculas;
+  final Function siguientePagina;
+
+  MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
+
+  // Seteando controlador de la pageView
+  final _pageController = new PageController(
+    initialPage: 1, 
+    viewportFraction: 0.3
+    );
 
   @override
   Widget build(BuildContext context) {
+
+    // Obteniendo tamaño de la pantalla del dispositivo
     final _screenSize = MediaQuery.of(context).size;
+
+    // Obtener posicion en pixeles del pageController
+    _pageController.addListener(() {
+
+      if(_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200) {
+        siguientePagina();
+      }
+
+    });
+
+
     return Container(
       height: _screenSize.height * 0.3,
       child: PageView(
         pageSnapping: false,
         children: _tarjetas(context),
-        controller: PageController(initialPage: 1, viewportFraction: 0.3),
+        controller:_pageController,
       ),
     );
   }
