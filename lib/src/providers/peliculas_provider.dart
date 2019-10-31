a17996d639440cae2aabc1f6d803a1c81eb6e016
bgarcia95 <bgarcia.95@outlook.com>
2019-10-31 00:25:22 -0600

    App finished

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -8,7 +8,6 @@ import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:peliculas/src/models/actores_model.dart';
 
 class PeliculasProvider {
-
   String _apikey = 'f4d30dfea84b32afbba315fe5d1ed849';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
@@ -20,17 +19,18 @@ class PeliculasProvider {
   // Streams, lo que transmitira es un listado de  peliculas
   List<Pelicula> _populares = new List();
 
-  final _popularesStreamController = new StreamController<List<Pelicula>>.broadcast();
+  final _popularesStreamController =
+      new StreamController<List<Pelicula>>.broadcast();
 
   void disposeStreams() {
     _popularesStreamController?.close();
   }
 
+  Function(List<Pelicula>) get popularesSink =>
+      _popularesStreamController.sink.add;
 
-  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
-
-  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
-
+  Stream<List<Pelicula>> get popularesStream =>
+      _popularesStreamController.stream;
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
@@ -43,52 +43,58 @@ class PeliculasProvider {
 
   Future<List<Pelicula>> getEnCines() async {
     // Construir  URL para realizar peticion http
-    final url = Uri.https(_url, '3/movie/now_playing',{
-      'api_key' : _apikey,
-      'language' : _language
-    });
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apikey, 'language': _language});
 
     return await _procesarRespuesta(url);
   }
 
   Future<List<Pelicula>> getPopulares() async {
-    
-    if(_cargando) return [];
+    if (_cargando) return [];
 
-    _cargando = true; 
-    _popularesPage++;  
+    _cargando = true;
+    _popularesPage++;
 
     // print('Cargando siguientes...');
 
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key': _apikey,
-      'language' : _language,
-      'page' : _popularesPage.toString()
+      'language': _language,
+      'page': _popularesPage.toString()
     });
 
     final resp = await _procesarRespuesta(url);
 
     _populares.addAll(resp);
     popularesSink(_populares);
-    
+
     _cargando = false;
 
     return resp;
   }
 
   Future<List<Actor>> getActores(String peliculaId) async {
-    final url = Uri.https(_url, '3/movie/$peliculaId/credits', {
-      'api_key': _apikey,
-      'language' : _language
-    });
+    final url = Uri.https(_url, '3/movie/$peliculaId/credits',
+        {'api_key': _apikey, 'language': _language});
 
     final resp = await http.get(url);
     final decodedData = json.decode(resp.body);
 
     final cast = new Cast.fromJsonList(decodedData['cast']);
-    
-    return cast.actores;
 
+    return cast.actores;
   }
 
+  // Para buscar Peliculas en MovieDB Api
+
+  Future<List<Pelicula>> buscarPelicula(String query) async {
+    // Construir  URL para realizar peticion http
+    final url = Uri.https(_url, '3/search/movie', {
+          'api_key': _apikey, 
+          'language': _language, 
+          'query': query
+    });
+
+    return await _procesarRespuesta(url);
+  }
 }
--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
-class DataSearch extends SearchDelegate {
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
+class DataSearch extends SearchDelegate {
   String seleccion = '';
+  final peliculasProvider = new PeliculasProvider();
 
   final peliculas = [
     'Spiderman',
@@ -60,28 +63,70 @@ class DataSearch extends SearchDelegate {
     );
   }
 
+  // @override
+  // Widget buildSuggestions(BuildContext context) {
+  //   // Son las sugerencias que aparecen cuando la persona escribe
+
+  //   final listaSugerida = (query.isEmpty)
+  //       ? peliculasRecientes
+  //       : peliculas
+  //           .where((pelicula) =>
+  //               pelicula.toLowerCase().startsWith(query.toLowerCase()))
+  //           .toList();
+
+  //   return ListView.builder(
+  //     itemCount: listaSugerida.length,
+  //     itemBuilder: (context, index) {
+  //       return ListTile(
+  //         leading: Icon(Icons.movie),
+  //         title: Text(listaSugerida[index]),
+  //         onTap: () {
+  //           seleccion = listaSugerida[index];
+  //           showResults(context);
+  //         },
+  //       );
+  //     },
+  //   );
+  // }
+
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
 
-    final listaSugerida = (query.isEmpty)
-        ? peliculasRecientes
-        : peliculas
-            .where((pelicula) =>
-                pelicula.toLowerCase().startsWith(query.toLowerCase()))
-            .toList();
+    if (query.isEmpty) return Container();
 
-    return ListView.builder(
-      itemCount: listaSugerida.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: Icon(Icons.movie),
-          title: Text(listaSugerida[index]),
-          onTap: () {
-            seleccion = listaSugerida[index];
-            showResults(context);
-          },
-        );
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        // Snapshot regresara un listado de la pelicula
+        if (snapshot.hasData) {
+          final peliculas = snapshot.data;
+
+          return ListView(
+            // Conviertiendo listado peliculas en una coleccion de widgets
+            children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: () {
+                  close(context, null);
+                  pelicula.uniqueId = '';
+                  Navigator.pushNamed(context, 'detalle_pelicula', arguments: pelicula);
+                },
+              );
+            }).toList(),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
       },
     );
   }
