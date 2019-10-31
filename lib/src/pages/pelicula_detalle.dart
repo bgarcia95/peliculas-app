72e1cf15cc0fd329620da551266324f2e695552b
bgarcia95 <bgarcia.95@outlook.com>
2019-10-30 18:25:38 -0600

    Adding CustomScrollView

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -11,7 +11,19 @@ class PeliculaDetalle extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _crearAppbar(pelicula),
-          
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10.0),
+                _posterTitulo(context, pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -36,6 +48,61 @@ class PeliculaDetalle extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 150),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              image: NetworkImage(pelicula.getPosterImg()),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(pelicula.title,
+                    style: Theme.of(context).textTheme.title,
+                    overflow: TextOverflow.ellipsis),
+                Text(pelicula.originalTitle,
+                    style: Theme.of(context).textTheme.subhead,
+                    overflow: TextOverflow.ellipsis),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Padding(
+                      padding: EdgeInsets.only(top: 2.0),
+                      child: Text(pelicula.voteAverage.toString(),
+                          style: Theme.of(context).textTheme.subhead),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            pelicula.overview,
+            textAlign: TextAlign.justify,
+          ),
+        ],
       ),
     );
   }
